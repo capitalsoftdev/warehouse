c1155478038704a76817e617f641911a9840bac2
Artur <capitalsoftdev@gmail.com>
2017-08-10 15:35:24 +0400

    commit3

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -15,6 +15,7 @@ namespace Test
             Console.WriteLine("sdfsdfdf");
             Console.WriteLine("sdfsdfdf");
             Console.WriteLine("sdfsdfdf");
+            Console.WriteLine("sdfsdfdf");
         }
     }
 }
