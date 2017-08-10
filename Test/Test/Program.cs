dca96053a797e2a689d52761f3b0ac74a2e68032
Artur <capitalsoftdev@gmail.com>
2017-08-10 15:33:29 +0400

    commit3

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -11,6 +11,10 @@ namespace Test
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+            Console.WriteLine("sdfsdfdf");
+            Console.WriteLine("sdfsdfdf");
+            Console.WriteLine("sdfsdfdf");
+            Console.WriteLine("sdfsdfdf");
         }
     }
 }
