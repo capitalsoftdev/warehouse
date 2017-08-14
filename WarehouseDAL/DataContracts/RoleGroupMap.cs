ff4ea9a509b3ceb73d001842822687eb415b529c
Artur <capitalsoftdev@gmail.com>
2017-08-14 21:04:47 +0400

    Comit1

---
+++ WarehouseDAL/DataContracts/RoleGroupMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseDAL.DataContracts
+{
+    public class RoleGroupMap
+    {
+        private int roleGroupId;
+        private int roleId;
+        public int RoleGroupId { get => roleGroupId; set => roleGroupId = value; }
+        public int RoleId { get => roleId; set => roleId = value; }
+    }
+}
