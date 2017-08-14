ba870c23d4594881b0c80fb33f4ede2f117ac751
Nastasya <Nastasya@DESKTOP-36129HG>
2017-08-14 21:01:16 +0400

    Product

---
+++ WarehouseDAL/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseDAL
+{
+    class Product
+    {
+        private int id;
+        private int productCategoryId;
+        private string name;
+        private int munit;
+        private bool isActive;
+        
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public int ProductCategoryId
+        {
+            get { return this.productCategoryId; }
+            set { this.productCategoryId = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Munit
+        {
+            get { return this.munit; }
+            set { this.munit = value; }
+        }
+
+        public bool IsActive
+        {
+            get { return this.isActive; }
+            set { this.isActive = value; }
+        }
+    }
+}
---
+++ WarehouseDAL/ProductAdaptor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseDAL
+{
+    class ProductAdaptor
+    {
+        
+        public int CreateOrUpdateProduct(Product product)
+        {
+            return 0;
+        }
+
+        public IList<Product> GetProduct()
+        {
+            return new List<Product>();
+        }
+
+        public Product GetProduct(int id)
+        {
+            return new Product();
+        }
+
+
+    }
+}
