b4be8362eaff38b15959d8239aed5b0278162ac5
Nastasya <Nastasya@DESKTOP-36129HG>
2017-08-15 14:57:38 +0400

    Procedure Product insert or update

--- WarehouseDAL/ProductManagmentAdapter.cs
+++ WarehouseDAL/ProductManagmentAdapter.cs
@@ -40,15 +40,15 @@ namespace WarehouseDAL
         private DateTime	lastModifyDate;
         private bool IsActive;
 
-        public int ProductId { get => productId; set => productId = value; }
-        public int Quantity { get => quantity; set => quantity = value; }
-        public DateTime ActionDate { get => actionDate; set => actionDate = value; }
-        public int Action { get => action; set => action = value; }
-        public int UserId { get => userId; set => userId = value; }
-        public decimal Price { get => price; set => price = value; }
-        public int SupplierId { get => supplierId; set => supplierId = value; }
-        public string Reason { get => reason; set => reason = value; }
-        public DateTime LastModifyDate { get => lastModifyDate; set => lastModifyDate = value; }
-        public bool IsActive1 { get => IsActive; set => IsActive = value; }
+        public int ProductId { get { return this.productId; } set { this.productId = value; } }
+        public int Quantity { get { return this.quantity; } set { quantity = value; } }
+        public DateTime ActionDate { get { return this.actionDate; } set { actionDate = value; } }
+        public int Action { get { return this.action; } set { this.action = value; } }
+        public int UserId { get { return this.userId; } set { this.userId = value; } }
+        public decimal Price { get { return this.price; } set { this.price = value; } }
+        public int SupplierId { get { return this.supplierId; } set { this.supplierId = value; } }
+        public string Reason { get { return this.reason; } set { this.reason = value; } }
+        public DateTime LastModifyDate { get { return this.lastModifyDate; } set { this.lastModifyDate = value; } }
+        public bool IsActive1 { get { return this.IsActive; } set { this.IsActive = value; } }
     }
 }
