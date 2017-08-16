74e896852d2d940869b87f4f3cdacd57abb0200c
ADT <ADT@DESKTOP-DK3JE2O>
2017-08-16 19:17:11 +0400

    User changes

--- WarehouseDAL/UserAdaptor.cs
+++ WarehouseDAL/UserAdaptor.cs
@@ -10,37 +10,93 @@ namespace WarehouseDAL
 {
     class UserAdaptor
     {
-        
-        private string _createUpdateUserSpName = "";
-        bool LoginUser(string login, string password) {
-            return false;
+        public IList<User> SelectActiveUser()
+        {
+            return GetActiveUsers(null);
         }
+        public User SelectActiveUser(int a)
+        {
+            var user = GetActiveUsers(a);
+            if (user.Count > 0)
+                return user[0];
+            return null;
+        }
+        private IList<User> GetActiveUsers(int? id)
+        {
+            IList<User> user = null;
+            using (var connection = new SqlConnection(ConnectionParameters.ConnectionString))
+            {
+                connection.Open();
+                using (var comand = new SqlCommand("SelectActiveUsers", connection))
+                {
+                    var param = new SqlParameter("@UserId", System.Data.SqlDbType.Int);
+                    if (id.HasValue)
+                    {
+                        param.Value = id.Value;
+                    }
+                    else
+                    {
+                        param.Value = DBNull.Value;
+                    }
+                    comand.Parameters.Add(param);
+                    SqlDataReader reader = comand.ExecuteReader();
+                    User use;
+                    user = new List<User>();
+                    while (reader.Read())
+                    {
+                        use = new User();
+                        use.Id = (Int32)reader["id"];
+                        use.Username = (string)reader["username"];
+                        use.Password = (string)reader["password"];
+                        use.RoleGroupId = (Int32)reader["roleGroupId"];
+                        use.CreationDate = (DateTime)reader["CreationDate"];
+                        if ((reader["lastLoginDate"]) == DBNull.Value)
+                            use.LastLoginDate = DateTime.MinValue;
+                        else use.LastLoginDate = (DateTime)(reader["lastLoginDate"]);
+                        use.LastModifireDate = (DateTime)reader["LastModifyDate"];
+                        user.Add(use);
+                    }
+                }
+            }
+            return user;
+        }
+      
+
+
 
         bool ManageUser(User user)
         {
+            int result = -1;
             bool retVal = false;
             using (var conn = new SqlConnection(ConnectionParameters.ConnectionString))
             {
                 conn.Open();
 
-                using (var cmd = new SqlCommand(_createUpdateUserSpName, conn))
+                using (var cmd = new SqlCommand(ConnectionParameters.ConnectionString, conn))
                 {
                     cmd.CommandType = System.Data.CommandType.StoredProcedure;
                     
-                    SqlParameter pUserName = new SqlParameter("userName", System.Data.SqlDbType.VarChar, 20);
+                    SqlParameter pUserName = new SqlParameter("@UserName", System.Data.SqlDbType.VarChar, 20);
                     pUserName.Value = user.Username;
                     cmd.Parameters.Add(pUserName);
 
-                    SqlParameter pResult = new SqlParameter("result", System.Data.SqlDbType.Int);
-                    pResult.Direction = System.Data.ParameterDirection.Output;
-                    cmd.Parameters.Add(pResult);
+                    SqlParameter pPassword = new SqlParameter("@Password", System.Data.SqlDbType.VarChar, 50);
+                    pPassword.Value = user.Password;
+                    cmd.Parameters.Add(pPassword);
+
+                    SqlParameter pRoleGroupId = new SqlParameter("@RoleGroupId", System.Data.SqlDbType.Int);
+                    pRoleGroupId.Value = user.RoleGroupId;
+                    cmd.Parameters.Add(pRoleGroupId);
+
+
+                   
 
                     cmd.ExecuteNonQuery();
 
 
-                    int result = Convert.ToInt32(pResult.Value);
+                    
 
-                    if (result == 1) return true;
+                    if (result == 0) return true;
                 }
                 return retVal;
             }
