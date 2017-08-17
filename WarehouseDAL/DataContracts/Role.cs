b38b6b947b254de679c6439f9887aa38db3635e7
NK <NK@DESKTOP-ACMRGJ9>
2017-08-17 17:43:23 +0400

    Role&RoleAdapter

---
+++ WarehouseDAL/DataContracts/Role.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContracts
+{
+    public class Role
+    {
+        private int id = -1;
+        private string name;
+        bool isActive;
+
+        public Role() { }
+        public Role(int id, string name, bool isActive)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.IsActive = isActive;
+        }
+
+        public int Id
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public bool IsActive
+        {
+            set { isActive = value; }
+            get { return isActive; }
+        }
+
+    }
+}
