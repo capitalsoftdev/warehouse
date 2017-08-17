d4c5d090aa837fcb2fcadb07b9504d22e3b8be96
diana1997 <israelyan.diana@mail.ru>
2017-08-17 18:16:33 +0400

    2017\08\17 Diana

--- DALTest/Program.cs
+++ DALTest/Program.cs
@@ -12,16 +12,42 @@ namespace DALTest
     {
         static void Main(string[] args)
         {
-            var product = new WarehouseDAL.DataContracts.Product();
-            product.Munit = 5;
-            product.Name = "Dianan kompy u gity ind kznvcyrala";
-            product.ProductCategoryId = 5;
-            
+             /*var product = new WarehouseDAL.DataContracts.Product();
+             product.Munit = 5;
+             product.Name = "Dianan kompy u gity ind kznvcyrala";
+             product.ProductCategoryId = 5;
 
-            var productAdaptor = new ProductAdaptor();
-            Console.WriteLine(productAdaptor.CreateOrUpdateProduct(product));
-            Console.ReadLine();
-            
+
+             var productAdaptor = new ProductAdaptor();
+             Console.WriteLine(productAdaptor.CreateOrUpdateProduct(product));*/
+
+            var prMn = new WarehouseDAL.DataContracts.ProductManagment();
+            /* prMn.Id = 15;
+             prMn.ProductId = 5;
+             prMn.Quantity = 888;
+             prMn.ActionDate = new DateTime(2021, 06, 4);
+             prMn.Action = 4;
+             prMn.UserId = 5;
+             prMn.Reason = "rea";
+             prMn.Price = 545;
+             prMn.SupplierId = 4;
+             prMn.Brand = "brand";
+             prMn.LastModifyDate = new DateTime(2014, 04, 22);
+             prMn.IsActive = true;
+             */
+             var prMnAdaptor = new ProductManagmentAdapter();
+            // Console.WriteLine(prMnAdaptor.CreateOrUpdate(prMn));
+
+            /* IList<WarehouseDAL.DataContracts.ProductManagment> list = prMnAdaptor.GetItem(1, 1, 1);
+             foreach(WarehouseDAL.DataContracts.ProductManagment l in list)
+             {
+                 Console.WriteLine(l);
+             }
+              Console.ReadLine();*/
+
+            Console.WriteLine(prMnAdaptor.DeleteItem(9));
+
+
 
         }
     }
