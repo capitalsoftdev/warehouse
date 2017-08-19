877524c1fb13efc131c608d2b6fd7fdcdde7df1b
liana1998 <mesropyan_liana@mail.ru>
2017-08-19 11:48:29 +0400

    commit19

--- DALTest/Program.cs
+++ DALTest/Program.cs
@@ -13,82 +13,83 @@ namespace DALTest
     {
         static void Main(string[] args)
         {
-            /*
-             var product = new WarehouseDAL.DataContracts.Product();
-             product.Munit = 5;
-             product.Name = "Dianan kompy u gity ind kznvcyrala";
-             product.ProductCategoryId = 5;
 
-
-            
-            var productAdaptor = new ProductAdaptor();
-            Console.WriteLine(productAdaptor.CreateOrUpdateProduct(product));
+            ///*
+            // var product = new WarehouseDAL.DataContracts.Product();
+            // product.Munit = 5;
+            // product.Name = "Dianan kompy u gity ind kznvcyrala";
+            // product.ProductCategoryId = 5;
 
 
 
-            var roleGroupMap = new WarehouseDAL.DataContracts.RoleGroupMap();
-            roleGroupMap.RoleGroupId = 15;
-            roleGroupMap.RoleId = 16;
-
-            var roleGroupMapAdapter = new RoleGroupMapAdapter();
-            IList<RoleGroupMap> list = roleGroupMapAdapter.GetRoleGroupI();
-
-            foreach (RoleGroupMap products in list)
-            {
-                Console.WriteLine(products);
-            }
-            IList<Product> list = productAdaptor.GetProduct();
-
-            foreach (Product products in list)
-             {
-                 Console.WriteLine(products);
-
-             }
-
-             Product productId = productAdaptor.GetProduct(1);
-             Console.WriteLine(productId);
+            //var productAdaptor = new ProductAdaptor();
+            //Console.WriteLine(productAdaptor.CreateOrUpdateProduct(product));
 
 
-             productAdaptor.DicebleProduct(1003);
 
-             list = productAdaptor.GetProduct();
+            //var roleGroupMap = new WarehouseDAL.DataContracts.RoleGroupMap();
+            //roleGroupMap.RoleGroupId = 15;
+            //roleGroupMap.RoleId = 16;
 
-             foreach (Product products in list)
-             {
-                 Console.WriteLine(products);
-             }
-             
+            //var roleGroupMapAdapter = new RoleGroupMapAdapter();
+            //IList<RoleGroupMap> list = roleGroupMapAdapter.GetRoleGroupI();
+
+            //foreach (RoleGroupMap products in list)
+            //{
+            //    Console.WriteLine(products);
+            //}
+            //IList<Product> list = productAdaptor.GetProduct();
+
+            //foreach (Product products in list)
+            // {
+            //     Console.WriteLine(products);
+
+            // }
+
+            // Product productId = productAdaptor.GetProduct(1);
+            // Console.WriteLine(productId);
 
 
-             var productAdaptor = new ProductAdaptor();
-             Console.WriteLine(productAdaptor.CreateOrUpdateProduct(product));
-            */
+            // productAdaptor.DicebleProduct(1003);
 
-            var prMn = new WarehouseDAL.DataContracts.ProductManagment();
-            /* prMn.Id = 15;
-             prMn.ProductId = 5;
-             prMn.Quantity = 888;
-             prMn.ActionDate = new DateTime(2021, 06, 4);
-             prMn.Action = 4;
-             prMn.UserId = 5;
-             prMn.Reason = "rea";
-             prMn.Price = 545;
-             prMn.SupplierId = 4;
-             prMn.Brand = "brand";
-             prMn.LastModifyDate = new DateTime(2014, 04, 22);
-             prMn.IsActive = true;
-             */
-             var prMnAdaptor = new ProductManagmentAdapter();
-            // Console.WriteLine(prMnAdaptor.CreateOrUpdate(prMn));
+            // list = productAdaptor.GetProduct();
 
-            /* IList<WarehouseDAL.DataContracts.ProductManagment> list = prMnAdaptor.GetItem(1, 1, 1);
-             foreach(WarehouseDAL.DataContracts.ProductManagment l in list)
-             {
-                 Console.WriteLine(l);
-             }
-              Console.ReadLine();*/
+            // foreach (Product products in list)
+            // {
+            //     Console.WriteLine(products);
+            // }
 
-            Console.WriteLine(prMnAdaptor.DeleteItem(9));
+
+
+            // var productAdaptor = new ProductAdaptor();
+            // Console.WriteLine(productAdaptor.CreateOrUpdateProduct(product));
+            //*/
+
+            //var prMn = new WarehouseDAL.DataContracts.ProductManagment();
+            ///* prMn.Id = 15;
+            // prMn.ProductId = 5;
+            // prMn.Quantity = 888;
+            // prMn.ActionDate = new DateTime(2021, 06, 4);
+            // prMn.Action = 4;
+            // prMn.UserId = 5;
+            // prMn.Reason = "rea";
+            // prMn.Price = 545;
+            // prMn.SupplierId = 4;
+            // prMn.Brand = "brand";
+            // prMn.LastModifyDate = new DateTime(2014, 04, 22);
+            // prMn.IsActive = true;
+            // */
+            // var prMnAdaptor = new ProductManagmentAdapter();
+            //// Console.WriteLine(prMnAdaptor.CreateOrUpdate(prMn));
+
+            ///* IList<WarehouseDAL.DataContracts.ProductManagment> list = prMnAdaptor.GetItem(1, 1, 1);
+            // foreach(WarehouseDAL.DataContracts.ProductManagment l in list)
+            // {
+            //     Console.WriteLine(l);
+            // }
+            //  Console.ReadLine();*/
+
+            //Console.WriteLine(prMnAdaptor.DeleteItem(9));
 
 
 
